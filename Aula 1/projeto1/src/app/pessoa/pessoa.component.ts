8f43e06ebda3efef641a258152321e94dd90c1c0
Angular <odair.viol@gmail.com>
2018-09-12 17:24:49 -0300

    Template html exemplo ngModel

--- Aula 1/projeto1/src/app/pessoa/pessoa.component.ts
+++ Aula 1/projeto1/src/app/pessoa/pessoa.component.ts
@@ -10,12 +10,7 @@ export class PessoaComponent implements OnInit {
 
   title = 'PIC Angular';
 
-  pessoas: Pessoa[] = [
-    new Pessoa("Odair Viol", 32),
-    new Pessoa("Roberto Bob", 30),
-    new Pessoa("Rodrigo Funny", 32),
-    new Pessoa("Dennis Garcia", 28)
-  ]
+  pessoas: Pessoa[] = []
 
   constructor() { }
 
