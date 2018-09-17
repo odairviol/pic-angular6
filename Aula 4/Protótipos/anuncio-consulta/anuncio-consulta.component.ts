2176e0a592c47ae1187fcc0b5fc53c2bab8f60fd
Angular <odair.viol@gmail.com>
2018-09-17 17:09:41 -0300

    Material aula 4

---
+++ Aula 4/Protótipos/anuncio-consulta/anuncio-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anuncio-consulta',
+  templateUrl: './anuncio-consulta.component.html',
+  styleUrls: ['./anuncio-consulta.component.css']
+})
+export class AnuncioConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
