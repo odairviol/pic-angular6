619a8073ec061cdeaef5fa9c23f90d40ee3c61eb
Angular <odair.viol@gmail.com>
2018-09-17 18:30:40 -0300

    Projeto Cast Anúncios inclusão de componentes base e rotas

--- Aula 4/cast-anuncios/src/app/app.module.ts
+++ Aula 4/cast-anuncios/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnuncioConsultaComponent } from './components/anuncios/anuncio-consulta/anuncio-consulta.component';
+import { AnuncioCadastroComponent } from './components/anuncios/anuncio-cadastro/anuncio-cadastro.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnuncioConsultaComponent,
+    AnuncioCadastroComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Aula 4/cast-anuncios/src/app/components/anuncios/anuncio-cadastro/anuncio-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anuncio-cadastro',
+  templateUrl: './anuncio-cadastro.component.html',
+  styleUrls: ['./anuncio-cadastro.component.css']
+})
+export class AnuncioCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
