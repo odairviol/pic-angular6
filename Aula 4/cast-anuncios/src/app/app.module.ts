cf9553351aa6d498b1cfd4fcb6a38129a9db68c7
Angular <odair.viol@gmail.com>
2018-09-17 20:05:42 -0300

    Inclusão chamada a API Fake com HttpClient e Json-Server

--- Aula 4/cast-anuncios/src/app/app.module.ts
+++ Aula 4/cast-anuncios/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AnuncioConsultaComponent } from './components/anuncios/anuncio-consulta/anuncio-consulta.component';
@@ -7,7 +9,6 @@ import { AnuncioCadastroComponent } from './components/anuncios/anuncio-cadastro
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-import { AppRoutingModule } from './app-routing.module';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
@@ -22,7 +23,8 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Aula 4/cast-anuncios/src/app/components/anuncios/anuncio-consulta/anuncio-consulta.component.ts
+++ Aula 4/cast-anuncios/src/app/components/anuncios/anuncio-consulta/anuncio-consulta.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TipoAnuncioService } from '../../../services/tipo-anuncio.service';
+import { TipoAnuncio } from '../../../models/tipo-anuncio.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-anuncio-consulta',
   templateUrl: './anuncio-consulta.component.html',
-  styleUrls: ['./anuncio-consulta.component.css']
+  styleUrls: ['./anuncio-consulta.component.css'],
+  providers: [TipoAnuncioService]
 })
 export class AnuncioConsultaComponent implements OnInit {
 
-  constructor() { }
+  tiposAnuncio: Observable<TipoAnuncio[]>;
+
+  constructor(private tipoAnuncioService: TipoAnuncioService) { }
 
   ngOnInit() {
+    this.tiposAnuncio = this.tipoAnuncioService.findAll();
   }
 
 }
